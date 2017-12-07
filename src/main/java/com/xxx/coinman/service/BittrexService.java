901c9a6dc16cd01f1b9ef4749cebcbffac0050ea
Administrator <Administrator@ADMIM>
2017-12-07 22:47:07 +0700

    zz

--- src/main/java/com/xxx/coinman/service/BittrexService.java
+++ src/main/java/com/xxx/coinman/service/BittrexService.java
@@ -4,8 +4,4 @@ public interface BittrexService {
     Double getPrice(String coinCode, String refCode) throws Exception;
     String buy(String coinCode, String refCode, String apiKey, double vol, double rate) throws Exception;
     String sell(String coinCode, String refCode, String apiKey, double vol, double rate) throws Exception;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> branch 'master' of https://github.com/hiepnvh/coinManager.git
